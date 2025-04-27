383954d5bc7ef4d04f28afc454d7e8716e3f7b68
bozadzhy <bozadzhy.f@gmail.com>
2025-04-27 13:41:51 +0300

    done with logic

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -9,8 +9,10 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { db } from "../firebase";
+import { useAuth } from "../hooks/use-auth";
 
 const ToDo = () => {
+  const { role } = useAuth();
   const [toDoName, setToDoName] = useState("");
   const [toDoList, setToDoList] = useState([]);
   const [taskTexts, setTaskTexts] = useState({});
@@ -256,24 +258,26 @@ const ToDo = () => {
 
   return (
     <div className="flex items-center flex-col mt-6">
-      <div className="flex items-center flex-col p-4 border rounded-lg mb-4">
-        <form onSubmit={addNewToDo} className="flex gap-2">
-          <input
-            value={toDoName}
-            onChange={(e) => setToDoName(e.target.value)}
-            className="flex-1 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-            type="text"
-            placeholder="Назва списку задач"
-            required
-          />
-          <button
-            type="submit"
-            className="bg-red-600 rounded-lg text-white border px-4 py-2 font-medium hover:bg-red-700 transition-colors"
-          >
-            + список
-          </button>
-        </form>
-      </div>
+      {role === "admin" && (
+        <div className="flex items-center flex-col p-4 border rounded-lg mb-4">
+          <form onSubmit={addNewToDo} className="flex gap-2">
+            <input
+              value={toDoName}
+              onChange={(e) => setToDoName(e.target.value)}
+              className="flex-1 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+              type="text"
+              placeholder="Назва списку задач"
+              required
+            />
+            <button
+              type="submit"
+              className="bg-red-600 rounded-lg text-white border px-4 py-2 font-medium hover:bg-red-700 transition-colors"
+            >
+              + список
+            </button>
+          </form>
+        </div>
+      )}
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {toDoList.map((toDo) => (
@@ -281,19 +285,25 @@ const ToDo = () => {
             key={toDo.id}
             className="bg-white rounded-lg shadow-md p-4 pt-12 mb-4 relative"
           >
-            <button
-              className="absolute top-2 right-2 w-8 h-8 flex items-center justify-center rounded-full text-white bg-red-600 text-lg"
-              onClick={() => handleDeleteToDo(toDo.id)}
-            >
-              ×
-            </button>
-            <div>
+            {role === "admin" && (
               <button
-                className="absolute top-2 right-12 w-8 h-8 flex items-center justify-center rounded-full text-white bg-blue-600 text-lg"
-                onClick={() => handleEditToDoText(toDo.id)}
+                className="absolute top-2 right-2 w-8 h-8 flex items-center justify-center rounded-full text-white bg-red-600 text-lg"
+                onClick={() => handleDeleteToDo(toDo.id)}
               >
-                ✎
+                ×
               </button>
+            )}
+
+            <div>
+              {role === "admin" && (
+                <button
+                  className="absolute top-2 right-12 w-8 h-8 flex items-center justify-center rounded-full text-white bg-blue-600 text-lg"
+                  onClick={() => handleEditToDoText(toDo.id)}
+                >
+                  ✎
+                </button>
+              )}
+
               {isEditTextToDo === toDo.id ? (
                 <div className="flex items-center justify-center gap-2 mb-4">
                   <input
@@ -312,6 +322,7 @@ const ToDo = () => {
                     className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
                     placeholder="редагувати назву списку задач"
                   />
+
                   <button
                     className="w-8 h-8 flex items-center justify-center rounded-full text-white bg-green-600 text-lg"
                     onClick={() => handleSaveToDoText(toDo.id)}
@@ -363,6 +374,7 @@ const ToDo = () => {
                           className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
                           placeholder="Опис задачі"
                         />
+
                         <button
                           className="w-8 h-8 flex items-center justify-center rounded-full text-white bg-green-600 text-lg self-end"
                           onClick={() => handleSaveTaskText(toDo.id, index)}
@@ -386,31 +398,33 @@ const ToDo = () => {
                             {task.description}
                           </span>
                         </div>
-                        <div className="flex items-center gap-2">
-                          <button
-                            className="w-8 h-8 flex items-center justify-center rounded-full text-white bg-green-600 text-lg"
-                            onClick={() => {
-                              handleEditTaskText(toDo.id, index);
-                              setTaskTexts((prev) => ({
-                                ...prev,
-                                [toDo.id]: {
-                                  name: task.name,
-                                  description: task.description,
-                                },
-                              }));
-                            }}
-                          >
-                            ✎
-                          </button>
-                          <button
-                            className="w-8 h-8 flex items-center justify-center rounded-full text-red-600 text-4xl"
-                            onClick={() => {
-                              handleDeleteTask(toDo.id, index);
-                            }}
-                          >
-                            ×
-                          </button>
-                        </div>
+                        {role === "admin" && (
+                          <div className="flex items-center gap-2">
+                            <button
+                              className="w-8 h-8 flex items-center justify-center rounded-full text-white bg-green-600 text-lg"
+                              onClick={() => {
+                                handleEditTaskText(toDo.id, index);
+                                setTaskTexts((prev) => ({
+                                  ...prev,
+                                  [toDo.id]: {
+                                    name: task.name,
+                                    description: task.description,
+                                  },
+                                }));
+                              }}
+                            >
+                              ✎
+                            </button>
+                            <button
+                              className="w-8 h-8 flex items-center justify-center rounded-full text-red-600 text-4xl"
+                              onClick={() => {
+                                handleDeleteTask(toDo.id, index);
+                              }}
+                            >
+                              ×
+                            </button>
+                          </div>
+                        )}
                       </div>
                     )}
                   </div>
@@ -419,85 +433,91 @@ const ToDo = () => {
                 <p className="text-gray-500">Поки немає завдань</p>
               )}
             </div>
-
-            <div className="flex items-center flex-col mt-4 gap-2">
-              <input
-                type="text"
-                className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
-                placeholder="Назва задачі"
-                value={taskTexts[toDo.id]?.name || ""}
-                onChange={(e) =>
-                  setTaskTexts((prev) => ({
-                    ...prev,
-                    [toDo.id]: { ...prev[toDo.id], name: e.target.value },
-                  }))
-                }
-              />
-
-              <input
-                type="text"
-                className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
-                placeholder="Опис задачі"
-                value={taskTexts[toDo.id]?.description || ""}
-                onChange={(e) =>
-                  setTaskTexts((prev) => ({
-                    ...prev,
-                    [toDo.id]: {
-                      ...prev[toDo.id],
-                      description: e.target.value,
-                    },
-                  }))
-                }
-              />
-
-              <button
-                onClick={() => {
-                  const task = taskTexts[toDo.id];
-                  if (task?.name?.trim()) {
-                    addTaskToToDo(toDo.id, task);
+            {role === "admin" && (
+              <div className="flex items-center flex-col mt-4 gap-2">
+                <input
+                  type="text"
+                  className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+                  placeholder="Назва задачі"
+                  value={taskTexts[toDo.id]?.name || ""}
+                  onChange={(e) =>
                     setTaskTexts((prev) => ({
                       ...prev,
-                      [toDo.id]: { name: "", description: "" },
-                    }));
+                      [toDo.id]: { ...prev[toDo.id], name: e.target.value },
+                    }))
                   }
-                }}
-                className="w-full bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded"
-              >
-                + Додати завдання
-              </button>
-            </div>
-            <div className="mt-4 flex flex-col gap-2">
-              <select
-                value={selectedUsers[toDo.id] || ""}
-                onChange={(e) =>
-                  setSelectedUsers((prev) => ({
-                    ...prev,
-                    [toDo.id]: e.target.value,
-                  }))
-                }
-                className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
-              >
-                <option value="">Вибрати співучасника</option>
-                {allUsers.map((user) => (
-                  <option key={user.id} value={user.email}>
-                    {user.email}
-                  </option>
-                ))}
-              </select>
+                />
 
-              <button
-                onClick={() => {
-                  const selectedUserEmail = selectedUsers[toDo.id];
-                  if (selectedUserEmail) {
-                    addUserToToDo(toDo.id, selectedUserEmail);
-                  } else {
-                    alert("Будь ласка, виберіть користувача");
+                <input
+                  type="text"
+                  className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+                  placeholder="Опис задачі"
+                  value={taskTexts[toDo.id]?.description || ""}
+                  onChange={(e) =>
+                    setTaskTexts((prev) => ({
+                      ...prev,
+                      [toDo.id]: {
+                        ...prev[toDo.id],
+                        description: e.target.value,
+                      },
+                    }))
                   }
-                }}
-                className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
-              >
-                + Додати співучасника
-              </button>
+                />
+                {role === "admin" && (
+                  <button
+                    onClick={() => {
+                      const task = taskTexts[toDo.id];
+                      if (task?.name?.trim()) {
+                        addTaskToToDo(toDo.id, task);
+                        setTaskTexts((prev) => ({
+                          ...prev,
+                          [toDo.id]: { name: "", description: "" },
+                        }));
+                      }
+                    }}
+                    className="w-full bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded"
+                  >
+                    + Додати завдання
+                  </button>
+                )}
+              </div>
+            )}
+
+            <div className="mt-4 flex flex-col gap-2">
+              {role === "admin" && (
+                <select
+                  value={selectedUsers[toDo.id] || ""}
+                  onChange={(e) =>
+                    setSelectedUsers((prev) => ({
+                      ...prev,
+                      [toDo.id]: e.target.value,
+                    }))
+                  }
+                  className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+                >
+                  <option value="">Вибрати співучасника</option>
+                  {allUsers.map((user) => (
+                    <option key={user.id} value={user.email}>
+                      {user.email}
+                    </option>
+                  ))}
+                </select>
+              )}
+              {role === "admin" && (
+                <button
+                  onClick={() => {
+                    const selectedUserEmail = selectedUsers[toDo.id];
+                    if (selectedUserEmail) {
+                      addUserToToDo(toDo.id, selectedUserEmail);
+                    } else {
+                      alert("Будь ласка, виберіть користувача");
+                    }
+                  }}
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
+                >
+                  + Додати співучасника
+                </button>
+              )}
 
               {toDo.users && toDo.users.length > 0 && (
                 <div className="mt-2">
